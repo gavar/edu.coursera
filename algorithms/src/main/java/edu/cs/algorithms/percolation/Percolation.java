d28084962392a7bd8477549fa4a0f2ab2f67b522
Max Stankevich <max.stankevich.me@gmail.com>
2018-10-28 21:32:13 +0200

    feat: algorithms percolation source files

---
+++ algorithms/src/main/java/edu/cs/algorithms/percolation/Percolation.java
@@ -0,0 +1,34 @@
+package edu.cs.algorithms.percolation;
+
+public class Percolation {
+
+    /** Create N-by-N grid, with all sites blocked. */
+    public Percolation(int n) {
+
+    }
+
+    /** Open site if it is not open already. */
+    public void open(int row, int col) {
+
+    }
+
+    /** Check if site is open. */
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    /** Check if site is full. */
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    /** Number of open sites. */
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    /** Check whether system percolates. */
+    public boolean percolates() {
+        return false;
+    }
+}
