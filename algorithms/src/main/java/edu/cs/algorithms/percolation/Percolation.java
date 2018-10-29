7308050693947acf03c45a8e7547b99862f710b4
Max Stankevich <max.stankevich.me@gmail.com>
2018-10-29 16:51:53 +0200

    feat: percolation without backwash

--- algorithms/src/main/java/edu/cs/algorithms/percolation/Percolation.java
+++ algorithms/src/main/java/edu/cs/algorithms/percolation/Percolation.java
@@ -3,14 +3,12 @@ package edu.cs.algorithms.percolation;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private static final int WORD = 8;
 
-    private static final int TOP = 0;
-    private static final int BOTTOM = 1;
-    private static final int WORD = 64;
-
-    private int open;
+    private int count = 0;
     private final int size;
-    private final long[] flags;
+    private final byte[] open;
+    private final byte[] flow;
     private final WeightedQuickUnionUF uf;
 
     /** Create N-by-N grid, with all sites blocked. */
@@ -18,84 +16,108 @@ public class Percolation {
         if (n <= 0)
             throw new IllegalArgumentException("N should be greater than zero!");
 
-        open = 0;
+        int memory = n * n + 1;
+        int bytes = (memory + WORD - 1) / WORD;
+
         size = n;
-        flags = new long[(n * n + WORD - 1) / WORD];
-        uf = new WeightedQuickUnionUF(n * n + 2);
+        open = new byte[bytes];
+        flow = new byte[bytes];
+        uf = new WeightedQuickUnionUF(memory);
     }
 
     /** Open site if it is not bits already. */
     public void open(int row, int col) {
-        // open cell
-        int cell = cellOf(row, col);
-        if (!setFlag(cell)) return; // do not open twice
-        open++;
+        // mark as open
+        int index = indexOf(row, col);
+        if (!setFlag(open, index)) return;
+        count++;
 
-        // connect to virtual TOP / BOTTOM
-        if (row == 1) uf.union(cell, size * size + TOP);
-        if (row == size) uf.union(cell, size * size + BOTTOM);
+        // remember if current component connected to bottoms
+        int root = uf.find(index);
+        boolean flowing = getFlag(flow, root);
 
-        // connect to adjacent
-        connect(cell, row - 1, col);
-        connect(cell, row + 1, col);
-        connect(cell, row, col - 1);
-        connect(cell, row, col + 1);
+        // connect to virtual TOP
+        if (row == 1)
+            uf.union(0, index);
+
+        // connect to adjacent and track if any cell connected to bottom
+        if (connect(index, row, col - 1)) flowing = true;
+        if (connect(index, row, col + 1)) flowing = true;
+        if (connect(index, row - 1, col)) flowing = true;
+        if (connect(index, row + 1, col)) flowing = true;
+
+        // if any of the component node connects to bottom,
+        // then every component node connects to the bottom
+        if (flowing || row == size) {
+            root = uf.find(index);
+            setFlag(flow, root);
+        }
     }
 
     /** Check if site is bits. */
     public boolean isOpen(int row, int col) {
-        int cell = cellOf(row, col);
-        return getFlag(cell);
+        int index = indexOf(row, col);
+        return getFlag(open, index);
     }
 
     /** Check if site is full. */
     public boolean isFull(int row, int col) {
-        int cell = cellOf(row, col);
-        return uf.connected(cell, size * size + TOP);
+        int index = indexOf(row, col);
+        return uf.connected(0, index);
     }
 
     /** Number of bits sites. */
     public int numberOfOpenSites() {
-        return open;
+        return count;
     }
 
     /** Check whether system percolates. */
     public boolean percolates() {
-        return uf.connected(size * size + TOP, size * size + BOTTOM);
+        int root = uf.find(0);
+        return getFlag(flow, root);
     }
 
-    private void connect(int cell, int row, int col) {
-        if (row < 1 || row > size) return;
-        if (col < 1 || col > size) return;
-        int adjacent = cellOf(row, col);
-        if (getFlag(adjacent))
-            uf.union(cell, adjacent);
+    private boolean connect(int index, int row, int col) {
+        if (row < 1 || row > size) return false;
+        if (col < 1 || col > size) return false;
+        return connect(index, indexOf(row, col));
     }
 
-    private boolean getFlag(int cell) {
-        final int index = cell / WORD;
-        final long word = flags[index];
-        final long mask = 1L << (cell % WORD);
+    private boolean connect(int index, int adjacent) {
+        // connect only with open cells
+        // can't be connected to bottom if closed
+        if (!getFlag(this.open, adjacent))
+            return false;
+
+        // connect to root since we already found it making tree more compact
+        int root = uf.find(adjacent);
+        boolean flowing = getFlag(flow, root);
+        uf.union(index, root);
+        return flowing;
+    }
+
+    private static boolean getFlag(byte[] flags, int index) {
+        final int mask = 1 << (index % WORD);
+        final int word = flags[index / WORD];
         return (word & mask) != 0;
     }
 
-    private boolean setFlag(int cell) {
-        final int index = cell / WORD;
-        final long word = flags[index];
-        final long mask = 1L << (cell % WORD);
-        final long next = word | mask;
+    private static boolean setFlag(byte[] flags, int index) {
+        final int mask = 1 << index % WORD;
+        final int word = flags[index / WORD];
+        final int next = word | mask;
         if (next == word) return false;
-        flags[index] = next;
+        flags[index / WORD] = (byte) next;
         return true;
     }
 
-    private int cellOf(int row, int col) {
+    private int indexOf(int row, int col) {
         if (row < 1 || row > size)
             throw new IllegalArgumentException("row is out of bounds");
 
         if (col < 1 || col > size)
             throw new IllegalArgumentException("col is out of bounds");
 
-        return (row - 1) * size + (col - 1);
+        return (row - 1) * size + col; // (0;0) is virtual top
     }
 }
--- algorithms/src/test/java/edu/cs/algorithms/percolation/PercolationTest.java
+++ algorithms/src/test/java/edu/cs/algorithms/percolation/PercolationTest.java
@@ -1,11 +1,14 @@
 package edu.cs.algorithms.percolation;
 
+import edu.princeton.cs.algs4.StdIn;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,9 +36,14 @@ public class PercolationTest {
             "percolation/wayne98.txt",
     })
     public void shouldPercolate(String fileName) throws IOException {
-        Percolation percolation = fromFile(fileName);
+        Scanner scanner = scannerOf(fileName);
+        final int size = scanner.nextInt();
+        Percolation percolation = fill(scanner, size);
         boolean percolates = percolation.percolates();
+        boolean backWash = isBackWash(fileName, percolation, size);
+        if (!percolates || backWash) draw(percolation, size);
         assertTrue(percolates);
+        assertFalse(backWash);
     }
 
     @ParameterizedTest
@@ -48,28 +56,60 @@ public class PercolationTest {
             "percolation/input10-no.txt",
     })
     public void shouldNotPercolate(String fileName) throws IOException {
-        Percolation percolation = fromFile(fileName);
+        Scanner scanner = scannerOf(fileName);
+        final int size = scanner.nextInt();
+        Percolation percolation = fill(scanner, size);
         boolean percolates = percolation.percolates();
+        boolean backWash = isBackWash(fileName, percolation, size);
+        if (percolates || backWash) draw(percolation, size);
         assertFalse(percolates);
+        assertFalse(backWash);
+
     }
 
-    public Percolation fromFile(String fileName) throws IOException {
+    public boolean isBackWash(String fileName, Percolation p, int size) {
+        switch (fileName) {
+            case "percolation/input10.txt":
+                return isAnyFull(p, size, 1, 2, 8);
+            case "percolation/input20.txt":
+                return isAnyFull(p, size, 1);
+            case "percolation/input50.txt":
+                return isAnyFull(p, size, 1, 3, 5, 6, 8, 21, 22, 23, 24, 27, 28, 30, 32, 33, 34, 35, 37, 40, 41, 44, 45, 46, 47, 48, 49, 50);
+            case "percolation/sedgewick60.txt":
+                return isAnyFull(p, size, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37);
+        }
+        return false;
+    }
+
+    public boolean isAnyFull(Percolation p, int row, int... cols) {
+        for (int col : cols)
+            if (p.isFull(row, col))
+                return true;
+        return false;
+    }
+
+    public void draw(Percolation percolation, int size) {
+        PercolationVisualizer.draw(percolation, size);
+        StdIn.readLine();
+    }
+
+    public Scanner scannerOf(String fileName) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         URL url = classLoader.getResource(fileName);
-        try (Scanner scanner = new Scanner(url.openStream())) {
-            int open = 0;
-            int size = scanner.nextInt();
-            Percolation percolation = new Percolation(size);
-            while (scanner.hasNext()) {
-                int row = scanner.nextInt();
-                int col = scanner.nextInt();
-                boolean close = !percolation.isOpen(row, col);
-                percolation.open(row, col);
-                if (close) open++;
-                assertTrue(percolation.isOpen(row, col), "cell should be marked as open");
-                assertEquals(open, percolation.numberOfOpenSites(), "open sites number does not match");
-            }
-            return percolation;
+        return new Scanner(url.openStream());
+    }
+
+    public Percolation fill(Scanner scanner, int size) {
+        Set<Integer> sites = new HashSet<>();
+        Percolation percolation = new Percolation(size);
+        while (scanner.hasNext()) {
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+            percolation.open(row, col); // open
+            sites.add((row - 1) * size + (col - 1)); // track unique sites
+            assertTrue(percolation.isOpen(row, col), "site should be marked as open");
+            assertEquals(sites.size(), percolation.numberOfOpenSites(), "open sites number does not match");
         }
+        return percolation;
     }
 }
