8fd5ca2f1de6af42791a8240427f96d6d6e622a1
Max Stankevich <max.stankevich.me@gmail.com>
2018-10-30 00:26:03 +0200

    feat: collinear initial code

---
+++ algorithms/src/main/java/edu/cs/algorithms/collinear/BruteCollinearPoints.java
@@ -0,0 +1,19 @@
+package edu.cs.algorithms.collinear;
+
+public class BruteCollinearPoints {
+
+    /** Finds all line segments containing 4 points. */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /** Number of line segments. */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /** Line segments. */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
---
+++ algorithms/src/main/java/edu/cs/algorithms/collinear/FastCollinearPoints.java
@@ -0,0 +1,19 @@
+package edu.cs.algorithms.collinear;
+
+public class FastCollinearPoints {
+
+    /** Finds all line segments containing 4 points. */
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    /** Number of line segments. */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /** Line segments. */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
