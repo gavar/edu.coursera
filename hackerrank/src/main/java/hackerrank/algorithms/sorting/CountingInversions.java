603e0ebd015e7c7017efd12d7d55310fc7010a92
Max Stankevich <max.stankevich.me@gmail.com>
2019-01-06 21:04:32 +0200

    feat@HackerRank: CountingInversions

---
+++ hackerrank/src/main/java/hackerrank/algorithms/sorting/CountingInversions.java
@@ -0,0 +1,53 @@
+package hackerrank.algorithms.sorting;
+
+/**
+ * Merge Sort: Counting Inversions.
+ * https://www.hackerrank.com/challenges/ctci-merge-sort/problem
+ */
+class CountingInversions {
+
+    static long countInversions(int[] arr) {
+        long swaps = sort(arr, 0, arr.length, new int[arr.length]);
+        assert isSorted(arr);
+        return swaps;
+    }
+
+    private static long sort(int[] arr, int lo, int hi, int[] aux) {
+        long count = 0L;
+        if (lo < hi - 1) {
+            int half = (hi + lo) / 2;
+            count += sort(arr, lo, half, aux);
+            count += sort(arr, half, hi, aux);
+            count += merge(arr, lo, half, hi, aux);
+        }
+        return count;
+    }
+
+    private static long merge(int[] arr, int lo, int half, int hi, int[] aux) {
+        int i = lo;
+        int l = lo;
+        int h = half;
+        long count = 0;
+
+        while (l < half && h < hi) {
+            if (arr[l] > arr[h]) {
+                aux[i++] = arr[h++];
+                count += half - l;
+            } else {
+                aux[i++] = arr[l++];
+            }
+        }
+
+        System.arraycopy(arr, l, aux, i, half - l);
+        System.arraycopy(arr, h, aux, i, hi - h);
+        System.arraycopy(aux, lo, arr, lo, hi - lo);
+        return count;
+    }
+
+    private static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++)
+            if (array[i - 1] > array[i])
+                return false;
+        return true;
+    }
+}
---
+++ hackerrank/src/test/java/hackerrank/algorithms/sorting/CountingInversionsTest.java
@@ -0,0 +1,56 @@
+package hackerrank.algorithms.sorting;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountingInversionsTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void countingInversions(String fileName, long[] expects) throws IOException {
+        Problem[] problems = parse(fileName);
+        for (int i = 0; i < problems.length; i++){
+            Problem problem = problems[i];
+            long expect = expects[i];
+            long actual = CountingInversions.countInversions(problem.array);
+            assertEquals(expect, actual, "case #" + (i + 1));
+        }
+    }
+
+    private Problem[] parse(String fileName) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL url = classLoader.getResource(fileName);
+        Scanner scanner = new Scanner(url.openStream());
+
+        Problem[] problems = new Problem[scanner.nextInt()];
+        for (int n = 0; n < problems.length; n++) {
+            int[] array = new int[scanner.nextInt()];
+            for (int i = 0; i < array.length; i++)
+                array[i] = scanner.nextInt();
+
+            Problem problem = new Problem();
+            problem.array = array;
+            problems[n] = problem;
+        }
+
+        return problems;
+    }
+
+    private class Problem {
+        int array[];
+    }
+
+    private static Object[][] data() {
+        return new Object[][]{
+                new Object[]{"algorithms.sorting/counting-inversions-0.txt", new long[]{0, 4}},
+                new Object[]{"algorithms.sorting/counting-inversions-14.txt", new long[]{1, 6}},
+                new Object[]{"algorithms.sorting/counting-inversions-15.txt", new long[]{0, 3}},
+        };
+    }
+}
