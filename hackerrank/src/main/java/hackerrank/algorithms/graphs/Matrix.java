d74b2276befb223f2f6bfc2c8fb336a82b32aaaa
Max Stankevich <max.stankevich.me@gmail.com>
2019-01-09 18:03:32 +0200

    feat@hackerrank: Matrix Graph

---
+++ hackerrank/src/main/java/hackerrank/algorithms/graphs/Matrix.java
@@ -0,0 +1,55 @@
+package hackerrank.algorithms.graphs;
+
+import java.util.*;
+
+public class Matrix {
+
+    static int minTime(int[][] roads, int[] machines) {
+        int n = roads.length + 1;
+
+        // union-find
+        int[] uf = new int[n];
+        for (int i = 0; i < n; i++)
+            uf[i] = i;
+
+        // mark city with machines
+        boolean[] connections = new boolean[n];
+        for (int city : machines)
+            connections[city] = true;
+
+        // sort connections by their time ascending,
+        // so we could leave most expensive machines connected
+        Arrays.sort(roads, Collections.reverseOrder(Comparator.comparingInt(x -> x[2])));
+
+        int totalTime = 0;
+        for (int[] road : roads) {
+            int a = find(uf, road[0]);
+            int b = find(uf, road[1]);
+
+            // number of machines in segments after merging
+            int m = 0;
+            if (connections[a]) m++;
+            if (connections[b]) m++;
+
+            if (m > 1) {
+                // we don't want to connect regions
+                // to form connection having more then 1 machine
+                totalTime += road[2];
+            } else {
+                uf[b] = a;
+                connections[a] = m > 0;
+                connections[b] = m > 0;
+            }
+        }
+
+        return totalTime;
+    }
+
+    private static int find(int[] uf, int node) {
+        while (node != uf[node]) {
+            uf[node] = uf[uf[node]];
+            node = uf[node];
+        }
+        return node;
+    }
+}
---
+++ hackerrank/src/test/java/hackerrank/algorithms/graphs/MatrixTest.java
@@ -0,0 +1,63 @@
+package hackerrank.algorithms.graphs;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatrixTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void minTime(String fileName, int expect) throws IOException {
+        Problem problem = parse(fileName);
+        int actual = Matrix.minTime(problem.roads, problem.machines);
+        assertEquals(expect, actual);
+    }
+
+    private Problem parse(String fileName) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL url = classLoader.getResource(fileName);
+        Scanner sc = new Scanner(url.openStream());
+
+        int n = sc.nextInt(); // number of cities
+        int k = sc.nextInt(); // number of machines
+
+        int[][] roads = new int[n - 1][];
+        for (int i = 0; i < n - 1; i++)
+            roads[i] = new int[]{
+                    sc.nextInt(),
+                    sc.nextInt(),
+                    sc.nextInt(),
+            };
+
+        int[] machines = new int[k];
+        for (int i = 0; i < k; i++)
+            machines[i] = sc.nextInt();
+
+        Problem problem = new Problem();
+        problem.machines = machines;
+        problem.roads = roads;
+        return problem;
+    }
+
+    private class Problem {
+        int[][] roads;
+        int[] machines;
+    }
+
+    private static Object[][] data() {
+        return new Object[][]{
+                new Object[]{"algorithms.graphs/matrix-0.txt", 10},
+                new Object[]{"algorithms.graphs/matrix-1.txt", 55},
+                new Object[]{"algorithms.graphs/matrix-8.txt", 3105329},
+                new Object[]{"algorithms.graphs/matrix-9.txt", 1738531},
+                new Object[]{"algorithms.graphs/matrix-10.txt", 5},
+                new Object[]{"algorithms.graphs/matrix-11.txt", 8},
+        };
+    }
+}
