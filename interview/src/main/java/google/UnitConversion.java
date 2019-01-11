6d9d67b1f8ec32022ad67d1638c62a6669e2c744
Max Stankevich <max.stankevich.me@gmail.com>
2019-01-11 22:51:35 +0200

    feat: google phone interview solution

---
+++ interview/src/main/java/google/UnitConversion.java
@@ -0,0 +1,102 @@
+package google;
+
+import java.util.*;
+
+/**
+ * Phone interview question.
+ * Date: 31.10.2018
+ *
+ * Task:
+ * 12 EUR  in USD     => 14.16 USD
+ * 2 Yards in Meters  => 1.828 Meters
+ *
+ * Input 1: ratio records
+ * Source | Ratio  | Destination
+ * GBP    | 0.69   | USD
+ * Yard   | 1.09   | Meter
+ * EUR    | 0.0077 | YEN
+ * YEN    | 167.75 | GBP
+ *
+ * Input 2: batch of queries
+ * Source | Destination
+ * EUR    | USD
+ * Meter  | Yard
+ *
+ * Output:
+ * Source | Ratio | Destination
+ * EUR    | 0.89  | USD
+ * Meter  | 0.91  | Yard
+ */
+class UnitConversion {
+
+    static double[] convert(String[] sources, double[] ratios, String[] destinations, String[][] queries) {
+        Graph graph = new Graph();
+        for (int i = 0; i < sources.length; i++)
+            graph.connect(sources[i], ratios[i], destinations[i]);
+        return convert(graph, queries);
+    }
+
+    static double[] convert(Graph graph, String[][] queries) {
+        double[] output = new double[queries.length];
+        for (int i = 0; i < queries.length; i++)
+            output[i] = convert(graph, queries[i][0], queries[i][1]);
+        return output;
+    }
+
+    static double convert(Graph graph, String from, String to) {
+        Node node = graph.nodes.get(from);
+        if (node == null) return -1;
+        Set<Node> visits = new HashSet<>();
+        return dfs(node, to, 1.0, visits);
+    }
+
+    static double dfs(Node node, String to, double total, Set<Node> visits) {
+        if (node.unit.equals(to)) return total;
+        if (!visits.add(node)) return -1;
+
+        for (Ratio ratio : node.ratios) {
+            double r = dfs(ratio.to, to, total * ratio.value, visits);
+            if (r > 0) return r;
+        }
+
+        return -1;
+    }
+
+    static class Graph {
+        Map<String, Node> nodes = new HashMap<>();
+
+        Node require(String unit) {
+            Node node = nodes.get(unit);
+            if (node != null) return node;
+            node = new Node(unit);
+            nodes.put(unit, node);
+            return node;
+        }
+
+        void connect(String source, double ratio, String destination) {
+            Node a = require(source);
+            Node b = require(destination);
+            a.ratios.add(new Ratio(b, ratio));
+            b.ratios.add(new Ratio(a, 1.0 / ratio));
+        }
+    }
+
+    static class Node {
+        String unit;
+        List<Ratio> ratios = new ArrayList<>();
+
+        Node(String unit) {
+            this.unit = unit;
+        }
+    }
+
+    static class Ratio {
+        Node to;
+        double value;
+
+        Ratio(Node to, double value) {
+            this.to = to;
+            this.value = value;
+        }
+    }
+}
---
+++ interview/src/test/java/google/UnitConversionTest.java
@@ -0,0 +1,68 @@
+package google;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitConversionTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void convert(String fileName) throws IOException {
+        Problem p = parse(fileName);
+        double[] outputs = UnitConversion.convert(p.sources, p.ratios, p.destinations, p.queries);
+        for (int i = 0; i < p.expects.length; i++) {
+            double expect = p.expects[i];
+            double actual = (int) (outputs[i] * 100) * 0.01;
+            assertEquals(expect, actual);
+        }
+    }
+
+    private Problem parse(String fileName) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL url = classLoader.getResource(fileName);
+        Scanner sc = new Scanner(url.openStream());
+
+        int n = sc.nextInt();
+        Problem p = new Problem();
+        p.ratios = new double[n];
+        p.sources = new String[n];
+        p.destinations = new String[n];
+        for (int i = 0; i < n; i++) {
+            p.sources[i] = sc.next();
+            p.ratios[i] = sc.nextDouble();
+            p.destinations[i] = sc.next();
+        }
+
+        int q = sc.nextInt();
+        p.queries = new String[q][];
+        p.expects = new double[q];
+        for (int i = 0; i < q; i++) {
+            p.queries[i] = new String[2];
+            p.queries[i][0] = sc.next();
+            p.expects[i] = sc.nextDouble();
+            p.queries[i][1] = sc.next();
+        }
+
+        return p;
+    }
+
+    private class Problem {
+        String[] sources;
+        String[] destinations;
+        double[] ratios;
+        String[][] queries;
+        double expects[];
+    }
+
+    private static String[] data() {
+        return new String[]{
+                "google/unit-conversion-1.txt",
+        };
+    }
+}
