88e0c277a168e2fc3fc4dfffe1dd9c1ee12238a7
Max Stankevich <max.stankevich.me@gmail.com>
2020-09-10 21:59:03 +0300

    fix(toptal): improving original solution

--- interview/src/main/java/edu/toptal/D20200910/Task1.java
+++ interview/src/main/java/edu/toptal/D20200910/Task1.java
@@ -1,52 +1,34 @@
 package edu.toptal.D20200910;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Task1 {
     public int solution(String S, int[] X, int[] Y) {
-        int N = S.length();
-        Point[] points = new Point[N];
-        for (int i = 0; i < N; i++) {
-            points[i] = new Point();
-            points[i].name = S.charAt(i);
-            points[i].distance = X[i] * X[i] + Y[i] * Y[i];
+        SortedMap<Long, List<Character>> locations = new TreeMap<>();
+
+        for (int i = 0, N = S.length(); i < N; i++) {
+            long distance = X[i] * X[i] + Y[i] * Y[i];
+            List<Character> points = locations.get(distance);
+            if (points == null) {
+                points = new LinkedList<>();
+                locations.put(distance, points);
+            }
+            points.add(S.charAt(i));
         }
-        Arrays.sort(points);
 
         int total = 0;
-        Set<Character> names = new HashSet<>();
-
-        for (int i = 0; i < N; ) {
-            Point point = points[i++];
-            if (!names.add(point.name))
-                return total;
-
-            int count = 1;
-            while (i < N && points[i].distance == point.distance) {
-                if (!names.add(point.name)) return total;
-                count++;
-                i++;
-            }
-
-            total += count;
-        }
+        Set<Character> points = new HashSet<>();
+        for (List<Character> batch : locations.values())
+            if (merge(points, batch)) total += batch.size();
+            else break;
 
         return total;
     }
 
-    class Point implements Comparable<Point> {
-        char name;
-        long distance;
-
-        public int compareTo(Point o) {
-            return Long.compare(distance, o.distance);
-        }
+    private <T> boolean merge(Set<T> target, Iterable<T> items) {
+        for (T item : items)
+            if (!target.add(item))
+                return false;
+        return true;
     }
-
-    // class Location {
-    //     long distance;
-    //     List<String> points;
-    // }
 }
