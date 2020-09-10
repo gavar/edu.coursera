a4556ccfc4715777559e1d20c4b6bdb1abcc9392
Max Stankevich <max.stankevich.me@gmail.com>
2020-09-11 02:22:37 +0300

    feat(toptal): implementation of Task #3

--- interview/src/main/java/edu/toptal/D20200910/Task3.java
+++ interview/src/main/java/edu/toptal/D20200910/Task3.java
@@ -1,7 +1,58 @@
 package edu.toptal.D20200910;
 
 public class Task3 {
-    public int solution(int[] A) {
-        return 0;
+    public int solution(String S) {
+        int N = S.length();
+
+        // total numbers of 'a' in array
+        int total = 0;
+        for (int i = 0; i < N; i++)
+            if (S.charAt(i) == 'a')
+                total++;
+
+        // all possible permutations
+        if (total == 0)
+            return (N - 2) * (N - 1) / 2;
+
+        // not possible if has remainder
+        if (total % 3 > 0) return 0;
+        int count = total / 3;
+
+        // find leftmost bound
+        int L = find(S, count);
+        if (L < 0) return 0;
+
+        // count LEFT permutations
+        int LP = 1;
+        for (L++; S.charAt(L) != 'a'; L++)
+            LP++;
+
+        // find rightmost bound
+        int R = findLast(S, count);
+        if (R < 0) return 0;
+
+        // count RIGHT permutations
+        int RP = 1;
+        for (R--; S.charAt(R) != 'a'; R--)
+            RP++;
+
+        // RIGHT permutation for every LEFT permutation
+        return RP * LP;
+    }
+
+    private int find(String S, int repeats) {
+        for (int i = 0; i < S.length(); i++)
+            if (S.charAt(i) == 'a')
+                if (--repeats == 0)
+                    return i;
+        return -1;
+    }
+
+    private int findLast(String S, int repeats) {
+        for (int i = S.length() - 1; i >= 0; i--)
+            if (S.charAt(i) == 'a')
+                if (--repeats == 0)
+                    return i;
+        return -1;
     }
 }
--- interview/src/test/java/edu/toptal/D20200910/Task3Test.java
+++ interview/src/test/java/edu/toptal/D20200910/Task3Test.java
@@ -9,17 +9,76 @@ import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class Task3Test {
     @MethodSource("data")
-    @ParameterizedTest(name = "N = {0}")
-    public void solve(int[] A, int expect) {
+    @ParameterizedTest()
+    public void solve(String S, int expect) {
         Task3 solver = new Task3();
-        int actual = solver.solution(A);
+        int actual = solver.solution(S);
         assertEquals(expect, actual);
     }
 
     static Arguments[] data() {
         return new Arguments[]{
                 // examples
-                of(new int[]{}, 0),
+                of("babaa", 2),
+                of("ababa", 4),
+                of("aba", 0),
+                of("bbbbb", 6),
+                // custom
+                of("bbbbbb", 10),
+                of("abababababa", 4),
+                of("abbabbababbaba", 9),
+                of("abbabbbababbaba", 12),
         };
     }
 }
+
+// abababababa
+// 1. aba|baba|baba
+// 2. aba|babab|aba
+// 3. abab|aba|baba
+// 4. abab|abab|aba
+
+// abbabbababbaba
+// 1. abba|bbaba|bbaba
+// 2. abba|bbabab|baba
+// 3. abba|bbababb|aba
+// 4. abbab|baba|bbaba
+// 5. abbab|babab|baba
+// 6. abbab|bababb|aba
+// 7. abbabb|aba|bbaba
+// 8. abbabb|abab|baba
+// 9. abbabb|ababb|aba
+
+// abbabbbababbaba
+//  1. abba|bbbababb|aba
+//  2. abba|bbbabab|baba
+//  3. abba|bbbaba|bbaba
+//  4. abbab|bbababb|aba
+//  5. abbab|bbabab|baba
+//  6. abbab|bbaba|bbaba
+//  7. abbabb|bababb|aba
+//  8. abbabb|babab|baba
+//  9. abbabb|baba|bbaba
+// 10. abbabbb|ababb|aba
+// 11. abbabbb|abab|baba
+// 12. abbabbb|aba|bbaba
+
+// bbbbb
+// 1. b|bbb|b
+// 2. b|bb|bb
+// 3. b|b|bbb
+// 4. bb|bb|b
+// 5. bb|b|bb
+// 6. bbb|b|b
+
+// bbbbbb
+//  1. b|b|bbbb
+//  2. b|bb|bbb
+//  3. b|bbb|bb
+//  4. b|bbbb|b
+//  5. bb|b|bbb
+//  6. bb|bb|bb
+//  7. bb|bbb|b
+//  8. bbb|b|bb
+//  9. bbb|bb|b
+// 10. bbbb|b|b
